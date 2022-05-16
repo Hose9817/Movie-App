b50aaf2ebf902016eae51abc5395c0f65fa7ca03
Hose9817 <abdullayev6859@gmail.com>
2022-05-16 15:12:56 +0600

    refactoring code: write fnc like obj methods

--- js/script.js
+++ js/script.js
@@ -1,68 +1,83 @@
-let numberOfFilms;
+'use strict';
 
-function start() {
-    numberOfFilms = +prompt('How much films do you watch?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('How much films do you watch?', '');
-    }
-}
-
-start();
+// const genresList = ['drama', 'comedy', 'triller', 'horror'];
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function () {
+        personalMovieDB.count = +prompt('How much films do you watch?', '');
+
+        while (personalMovieDB.count === '' || personalMovieDB.count === null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('How much films do you watch?', '');
+        }
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Your last watching film?', ''),
+                b = +prompt('Rating of last film?', '0');
+            if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done loop');
+            } else {
+                console.log('error loop');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function () {
+        if (personalMovieDB.count < 10) {
+            console.log('You dont watching many movies');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('You are a classic viewer');
+        } else if (personalMovieDB.count > 30) {
+            console.log('You are a real film fan!');
+        } else {
+            console.log('Error');
+        }
+    },
+
+    showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+
+    writeYourGenres: function () {
+        for (let i = 1; i < 2; i++) {
+            let genres = prompt(`Your favorite film genres is?`).toLowerCase();
+            if (genres === '' || genres === null) {
+                console.log('Yoy messed up, asshole!');
+                i--;
+            } else {
+                personalMovieDB.genres = genres.split(', ').sort();
+            }
+        }
+        personalMovieDB.genres.forEach((el, i) => {
+            console.log(`Your favorite genres ${i+1} is ${el}`);
+        });
+    }
+
 };
 
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Your last watching film?', ''),
-            b = +prompt('Rating of last film?', '0');
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done loop');
-        } else {
-            console.log('error loop');
-            i--;
-        }
-    }
-}
+// personalMovieDB.start();
 
-// rememberMyFilms();
+// personalMovieDB.rememberMyFilms();
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('You dont watching many movies');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('You are a classic viewer');
-    } else if (personalMovieDB.count > 30) {
-        console.log('You are a real film fan!');
-    } else {
-        console.log('Error');
-    }
-}
+// personalMovieDB.detectPersonalLevel();
 
-// detectPersonalLevel();
+// personalMovieDB.writeYourGenres();
 
+// personalMovieDB.toggleVisibleMyDB();
 
-function writeYourGenres() {
-    for(let i = 0; i <= 2; i++) {
-       personalMovieDB.genres[i] = prompt(`Your favorite film genres is ${i+1}?`);
-    }
-}
-
-writeYourGenres();
-
-
-function showMyDB(hidden) {
-    if(!hidden){
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
-
+// personalMovieDB.showMyDB();
